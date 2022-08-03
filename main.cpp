60571333c99f7316264fd30520727055969812f7
Ruben Valle Ramirez <107521307+valle572@users.noreply.github.com>
2022-08-03 16:36:33 -0700

    Removed unnecessary logic, shortened code

--- main.cpp
+++ main.cpp
@@ -27,7 +27,6 @@ public:
     Node *left  = nullptr;
     Node *right = nullptr;
     int data;
-    ~Node() {};
 };
 
 class Tree {
@@ -38,14 +37,11 @@ public:
     Tree(int data) {  // Tree with one node
         root = insertData(root, data);
     }
-
     void printTree()           { Print(root); };
     void printTree(Node *node) { Print(node); };
     void freeAllocatedData()   { freeAllocatedData(root); };
     void addData(int data)     { root = insertData(root, data); };
     Node *searchFor(int data)  { return searchData(root, data); };
-
-    ~Tree() {};
 
 private:
     Node *root = nullptr;
@@ -55,46 +51,44 @@ private:
         if (node == nullptr) {        // Insert Data
             node = new Node(data);
             cout << "Inserted new data: " << data << "\n\n";
-            return node;
         }
-        if (data < node->data)       // Recurse left
+        else if (data < node->data)       // Recurse left
             node->left  = insertData(node->left, data);
 
-        if (data > node->data)      // Recurse right
+        else if (data > node->data)      // Recurse right
             node->right = insertData(node->right, data);
+
         return node;
     }
     Node *searchData(Node *node, int data) {
+        // Node is found or empty
+        if (!node || data == node->data) return node;
 
-        if (node == nullptr || data == node->data)
-            return node;                          // Node is found or empty
-
+        // Recurse left or right
         node = (data < node->data) ? node->left : node->right;
-        return searchData(node, data);            // Recurse left or right
+
+        return searchData(node, data);
     }
     void Print(Node *node) {
         try {
-            if (node == nullptr) throw "Tree is empty";
+            if (!node) throw "Tree is empty";
 
-            if (node->left  != nullptr) Print(node->left);  // Recurse left
+            if (node->left) Print(node->left);  // Recurse left
             cout << "Node Data: " << node->data << "\n";
-            if (node->right != nullptr) Print(node->right); // Recurse Right
+            if (node->right) Print(node->right); // Recurse Right
         }
-        catch (const char *e) {					  // Exception is thrown
+        catch (const char *e) {
             cout << "[Exception occured] " << e << "\n";
         }
     }
     void freeAllocatedData(Node *node) {
-        if (node == nullptr)
-            return;
-
+        if (!node) return;
         freeAllocatedData(node->left);
         freeAllocatedData(node->right);
         delete node;
     }
 };
 void BeginTests() {
-
     int  data[] = {-1, 6, 7, 1, 9, -5, 4},
          arraySize   = sizeof(data) / sizeof(int),
          MAX_SEARCH_VAL = 10;      // for search loop
@@ -102,20 +96,18 @@ void BeginTests() {
     PrintDivider();
     cout << "Adding data to the Tree . . .\n\n";
 
-    Tree tree(10);         // Tree with one Node
+    Tree tree(10);
     for (int i = 0; i < arraySize; i++)
         tree.addData(data[i]);
 
     PrintDivider();
     cout << "Printing data using In-Order Traversal . . .\n\n";
-
     tree.printTree();
 
     PrintDivider();
     cout << "Undergoing a series of searches . . .\n\n";
 
     for (int toFind = 5; toFind <= MAX_SEARCH_VAL; toFind++) {
-
         cout << "Looking for data: " << toFind << " . . .\n";
         bool found = tree.searchFor(toFind);
 
@@ -124,21 +116,14 @@ void BeginTests() {
         else
             cout << "[Not Found] Looked for: " << toFind << "\n\n";
     }
-
     PrintDivider();
     cout << "Testing exception handling . . .\n\n";
-    
-    Node *testNode = nullptr;
-    Tree testTree;
-
-    testTree.printTree(testNode); // Nullptr
-
+    tree.printTree(nullptr);
     cout << "\nException handling successful!\n\n";
 
     PrintDivider();
     cout << "Freeing Memory . . .\n\n";
     tree.freeAllocatedData();
-
 }
 int main() {
 
