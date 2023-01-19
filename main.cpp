63690937aa81166d7bd8db0838d9cebd13e11fb4
Ruben Valle Ramirez <rubenvalle93@gmail.com>
2023-01-19 01:01:05 -0700

    Update main.cpp
    Updated variable/function names & no longer using std namespace

--- main.cpp
+++ main.cpp
@@ -1,125 +1,132 @@
 #include <memory>
 #include <iostream>
-using namespace std;
+
 
 // Global variable to keep track of memory in use
 int usedMem = 0;
 
+
 void* operator new(size_t size) {
     usedMem += size;
-    cout << "[Allocating " << size << " bytes]\n";
+    std::cout << "[Allocating " << size << " bytes]\n";
     return malloc(size);
 }
-
-void operator delete(void* memPtr, size_t size) {
+void operator delete(void* memPtr, size_t size) noexcept {
     usedMem -= size;
-    cout << "[Freeing " << size << " bytes]\n";
+    std::cout << "[Freeing " << size << " bytes]\n";
     free(memPtr);
 }
+void printDiv() {
+    std::cout << "\n======================================================\n";
+};
 
-void printDiv() { cout << "________________________________________\n\n"; };
 
 class Node {
 public:
     Node(int nodeData) {
         data = nodeData;
     }
-    Node *left, *right;
+    Node *leftPtr, *rightPtr;
     int data;
 };
+
 
 class Tree {
 public:
 
     Tree() = default;
     Tree(int data) {
-        root = insert(root, data);
+        rootPtr = insert(rootPtr, data);
     }
-    Node *contains(int data)   { return search(root, data); };
-    void add(int data)         { root = insert(root, data); };
-    void freeMemory()         { freeMemory(root); };
-    void printTree()           { Print(root); };
-    void printTree(Node *node) { Print(node); };
+    Node *contains(int data)      { return search(rootPtr, data); };
+    void add(int data)            { rootPtr = insert(rootPtr, data); };
+    void clearTree()              { deleteNode(rootPtr); };
+    void printTree()              { print(rootPtr); };
+    void printTree(Node *nodePtr) { print(nodePtr); };
 
 private:
-    Node *root = nullptr;
+    Node *rootPtr = nullptr;
 
-    Node *insert(Node *node, int data) {
+    Node *insert(Node *nodePtr, int data) {
 
-        if (!node) {                // Insert data
-            node = new Node(data);
-            cout << "Inserted new data: " << data << "\n\n";
+        if (!nodePtr) {                // Insert data
+            nodePtr = new Node(data);
+            std::cout << "Inserted new data: " << data << "\n\n";
         }
-        else if (data < node->data) // Recurse left
-            node->left  = insert(node->left, data);
+        else if (data < nodePtr->data) // Recurse left
+            nodePtr->leftPtr  = insert(nodePtr->leftPtr, data);
 
-        else if (data > node->data) // Recurse right
-            node->right = insert(node->right, data);
+        else if (data > nodePtr->data) // Recurse right
+            nodePtr->rightPtr = insert(nodePtr->rightPtr, data);
 
-        return node;
+        return nodePtr;
     }
-    Node *search(Node *node, int target) {
-        if (!node || target == node->data) return node;
+    Node *search(Node *nodePtr, int num) {
+        if (!nodePtr || num == nodePtr->data) return nodePtr;
         
         // Recurse left or right
-        node = (target < node->data) ? node->left : node->right;
-        return search(node, target);
+        nodePtr = (num < nodePtr->data) ? nodePtr->leftPtr : nodePtr->rightPtr;
+        return search(nodePtr, num);
     }
-    void Print(Node *node) {
+    void print(Node *nodePtr) {
         try {
-            if (!node) throw "Tree is empty";
+            if (!nodePtr) throw "Tree is empty";
 
-            if (node->left)  Print(node->left);  // Recurse left
-            cout << "Node Data: " << node->data << "\n";
-            if (node->right) Print(node->right); // Recurse right
+            if (nodePtr->leftPtr)  print(nodePtr->leftPtr);  // Recurse left
+            std::cout << "Node Data: " << nodePtr->data << "\n";
+            if (nodePtr->rightPtr) print(nodePtr->rightPtr); // Recurse right
         }
         catch (const char *e) {
-            cout << "[Exception Occurred] " << e << "\n";
+            std::cout << "[Exception Occurred] " << e << "\n";
         }
     }
-    void freeMemory(Node *node) {
-        if (!node) return;
-        freeMemory(node->left);
-        freeMemory(node->right);
-        delete node;
+    void deleteNode(Node *nodePtr) {
+        if (!nodePtr) return;
+        deleteNode(nodePtr->leftPtr);
+        deleteNode(nodePtr->rightPtr);
+        delete nodePtr;
     }
 };
+
+
 void BeginTests() {
     int  dataArr[] = {-1, 6, 7, 1, 9, -5, 4};
 
     printDiv();
-    cout << "Adding data to the tree . . .\n\n";
+    std::cout << "Adding data to the tree . . .\n\n";
     Tree tree(10);
     for (int data : dataArr) tree.add(data);
 
     printDiv();
-    cout << "Printing data using in-order traversal . . .\n\n";
+    std::cout << "Printing data using in-order traversal . . .\n\n";
     tree.printTree();
 
     printDiv();
-    cout << "Searching for data: 1-10 . . .\n\n";
-    for (int target = 0; target <= 10; target++) {
-        string found = tree.contains(target) ? "yes" : "no";
-        cout << "Tree has " << target << ": " << found << "\n";
+    std::cout << "Searching for data: 0-9 . . .\n\n";
+    for (int target = 0; target < 10; target++) {
+        std::string found = tree.contains(target) ? "yes" : "no";
+        std::cout << "Tree has " << target << ": " << found << "\n";
     }
 
     printDiv();
-    cout << "Testing exception handling . . .\n\n";
+    std::cout << "Testing exception handling . . .\n\n";
     tree.printTree(nullptr);
-    cout << "\nSuccess!\n";
+    std::cout << "\nSuccess!\n";
 
     printDiv();
-    cout << "Freeing memory . . .\n\n";
-    tree.freeMemory();
+    std::cout << "Freeing memory . . .\n\n";
+    tree.clearTree();
 }
+
+
 int main() {
 
     BeginTests();
     
     printDiv();
-    cout << "\nMemory usage: " << usedMem << " Bytes\n\n";
+    std::cout << "\nMemory usage: " << usedMem << " bytes\n\n";
     printDiv();
-    cout << "Tests completed!\n\n";
+    std::cout << "Tests completed!\n\n";
 
     return 0;
 }
